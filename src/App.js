1ef3320e15aa2791e3b18ce5347c285681a9aeeb
Izet Molla <izetmolla@Izets-MacBook-Air.local>
2022-05-31 22:48:21 +0200

    new edited files

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ function App() {
   const [content] = useState([
     { photoSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80', name: 'User 1', comment: 'test comment', time: "today at 3" },
     { photoSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80', name: 'User 2', comment: 'test comment', time: "today at 4" },
-    { photoSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80', name: 'User 3', comment: 'test comment', time: "today at 5" }
+    { photoSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80', name: 'User 3', comment: 'test comment', time: "today at 5" },
+    { photoSrc: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80', name: 'User 4', comment: 'test comment', time: "today at 5" }
   ])
 
   return (
-    <div style={{ flex: 1, width: "100%" }}>
-      <div style={{ alignContent: "center", alignItems: "center", justifyContent: "center" }}>
+    <div style={{ flex: 1, width: "100%", textAlign: "justify" }}>
+      <center>
         {content.map((item, i) => {
           return (
             <div key={i} style={{
-              alignSelf: "center",
+              alignItems: "center",
               margin: 20,
               maxWidth: 400,
               borderRadius: 10,
-              flexDirection: "row", border: 1, border: '1px solid black'
+              flexDirection: "row", border: 1, border: '1px solid black',
             }}>
               <div style={{ display: "flex", flexDirection: "row", padding: 10, }}>
                 <img src={item.photoSrc} style={{ widht: 40, height: 40 }} />
@@ -30,13 +31,32 @@ function App() {
               </div>
               <hr />
               <div style={{ display: 'flex', justifyContent: "center", padding: 10 }}>
-                <button>Approve</button>
-                <button>Reject</button>
+                <button style={{
+                  elevation: 8,
+                  width: 100,
+                  padding: 10,
+                  borderRadius: 5,
+                  borderColor: "green"
+                  , backgroundColor: "transparent",
+                  color: "green",
+                  margin: 1
+                }}>Approve</button>
+                <button
+                  style={{
+                    margin: 1,
+                    elevation: 8,
+                    width: 100,
+                    padding: 10,
+                    borderRadius: 5,
+                    borderColor: "red"
+                    , backgroundColor: "transparent",
+                    color: "red"
+                  }}>Reject</button>
               </div>
             </div>
           )
         })}
-      </div>
+      </center>
     </div>
 
   );
